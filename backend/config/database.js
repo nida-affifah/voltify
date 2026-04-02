cf1e4b13aa35c3d3bf286a875f6bcffae309bc5b
nida-affifah <nidaduwaa@gmail.com>
2026-04-02 11:03:04 +0700

    Fix vercel config

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,24 +2,8 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    max: 20,
-    idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 2000,
-});
-
-// Test connection
-pool.connect((err, client, release) => {
-    if (err) {
-        console.error('Database connection error:', err.stack);
-    } else {
-        console.log('PostgreSQL connected successfully');
-        release();
-    }
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
